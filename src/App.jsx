31e264e498da6b66e076914b6072828134cc0979
deyas1 <deyaschaudhary14@gmail.com>
2026-06-12 16:52:24 +0545

    Fix component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
